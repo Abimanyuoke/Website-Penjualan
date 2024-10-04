50dcbcfeb1a5b032e5b2f78de9fd1c10d57a9787
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-10-04 22:35:41 +0700

    add search box

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ document.querySelector('#search-button').onclic = (e) => {
     searchBox.focus()
     e.preventDefault()
 }
+
+// Klik di luar elemen
+const hamburger = document.querySelector('#hamburger-menu');
+const searchButton = document.querySelector('#search-button')
+
+document.addEventListener('click', function(e) {
+    if(hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+
+    if(searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+})
