b5050df64fd87d2acc9548454a5abc2a0a8d1ea9
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-10-04 23:30:10 +0700

    add tggle active, add img produk, and add produk

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-// Toogle class active untuk search form
+// Toggle class active untuk hamburger manu 
+const navbarNav = document.querySelector('.navbar-nav');
+
+// Ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active')
+}
+
+
+
+// Toggle class active untuk search form
 const searchForm = document.querySelector('.search-form');
 const searchBox = document.querySelector('#search-box');
 
-document.querySelector('#search-button').onclic = (e) => {
-    searchForm.classList.toogle('active');
-    searchBox.focus()
-    e.preventDefault()
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
 }
 
 // Klik di luar elemen
