4b0cf6923eb0ecaefd2a6cb4740006c87fc8828a
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-10-04 23:33:40 +0700

    fix the problem

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const searchForm = document.querySelector('.search-form');
 const searchBox = document.querySelector('#search-box');
 
 document.querySelector('#search-button').onclick = (e) => {
-    searchForm.classList.toggle('active');
+    searchForm.classList.toggle('abim');
     searchBox.focus();
     e.preventDefault();
 }
