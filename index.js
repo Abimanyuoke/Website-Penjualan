c934985bd236dda4287ff02fde8bf45765f2d3df
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-10-05 22:13:09 +0700

    add modal

--- index.js
+++ index.js
@@ -23,22 +23,47 @@ document.querySelector('#shopping-cart-button').onclick = (e) => {
     e.preventDefault();
 }
 
+// Modal
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    }
+})
+
+
+// klik tombol close modal
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+}
+
 
 // Klik di luar elemen
-// const hamburger = document.querySelector('#hamburger-menu');
-// const searchButton = document.querySelector('#search-button');
-// const shoppingButton = document.querySelector('#shopping-cart-button');
+const hamburger = document.querySelector('#hamburger-menu');
+const searchButton = document.querySelector('#search-button');
+const shoppingButton = document.querySelector('#shopping-cart-button');
 
-// document.addEventListener('click', function(e) {
-//     if(hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-//         navbarNav.classList.remove('active');
-//     }
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
 
-//     if(searchButton.contains(e.target) && !searchForm.contains(e.target)) {
-//         searchForm.classList.remove('active');
-//     }
+    if (!searchButton.contains(e.target) && !search.contains(e.target)) {
+        search.classList.remove('active');
+    }
 
-//     if(shoppingButton.contains(e.target) && !shoppingCart.contains(e.target)) {
-//         shoppingCart.classList.remove('active');
-//     }
-// })
+    if (!shoppingButton.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active');
+    }
+})
+
+// klik di luar modal
+    window.onclick = (e) => {
+        if (e.target === itemDetailModal) {
+            itemDetailModal.style.display = 'none';
+        }
+    }
