eb5043a83b6f15468b52594f093df9dac0e1e04d
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-10-05 20:09:09 +0700

    fix the problem js

--- index.js
+++ index.js
@@ -6,38 +6,39 @@ document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active')
 }
 
-// Toogle class active untuk produk
-const shoppingCart = document.querySelector('.shoping-cart');
-document.querySelector('#shopping-cart-button').onclick = (e) => {
-    shoppingCart.classList.toggle('angga');
-    e.preventDefault();
-}
-
 // Toggle class active untuk search form
-const searchForm = document.querySelector('.search-form');
+const search = document.querySelector('.search-form');
 const searchBox = document.querySelector('#search-box');
 
 document.querySelector('#search-button').onclick = (e) => {
-    searchForm.classList.toggle('abim');
+    search.classList.toggle('active');
     searchBox.focus();
     e.preventDefault();
 }
 
+// Toogle class active untuk produk
+const shoppingCart = document.querySelector('.shoping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+}
+
+
 // Klik di luar elemen
-const hamburger = document.querySelector('#hamburger-menu');
-const searchButton = document.querySelector('#search-button');
-const shoppingButton = document.querySelector('#shopping-cart-button');
+// const hamburger = document.querySelector('#hamburger-menu');
+// const searchButton = document.querySelector('#search-button');
+// const shoppingButton = document.querySelector('#shopping-cart-button');
 
-document.addEventListener('click', function(e) {
-    if(hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
-    }
+// document.addEventListener('click', function(e) {
+//     if(hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+//         navbarNav.classList.remove('active');
+//     }
 
-    if(searchButton.contains(e.target) && !searchForm.contains(e.target)) {
-        searchForm.classList.remove('abim');
-    }
+//     if(searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+//         searchForm.classList.remove('active');
+//     }
 
-    if(shoppingButton.contains(e.target) && !shoppingCart.contains(e.target)) {
-        shoppingCart.classList.remove('anggga');
-    }
-})
+//     if(shoppingButton.contains(e.target) && !shoppingCart.contains(e.target)) {
+//         shoppingCart.classList.remove('active');
+//     }
+// })
