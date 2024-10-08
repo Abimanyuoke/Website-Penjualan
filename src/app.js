bba563dbeb55755569dae9b74de5dce2590d10b8
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-10-08 12:45:32 +0700

    add price js, and make the alpine js

--- src/app.js
+++ src/app.js
@@ -9,3 +9,13 @@ document.addEventListener('alpine:init', () => {
         ]
     }))
 })
+
+// konversi rupiah
+
+const rupiah = (number) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits : 0,
+    }).format(number);
+}
