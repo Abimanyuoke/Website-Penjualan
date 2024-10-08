40b0b29ca56416a6d3caf4d4f72b4a0de56110c8
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-10-08 14:52:53 +0700

    add products

--- src/app.js
+++ src/app.js
@@ -8,6 +8,18 @@ document.addEventListener('alpine:init', () => {
             { id : 5, name : 'Sumatra Mandheling', img: '5.png', price: 40000 },
         ]
     }))
+
+
+    Alpine.store('cart', {
+        items: [],
+        total: 0,
+        quantity: 0,
+        add(newItem) {
+            this.items.push(newItem);
+            this.quantity++;
+            this.total += newItems.price;
+        }
+    })
 })
 
 // konversi rupiah
