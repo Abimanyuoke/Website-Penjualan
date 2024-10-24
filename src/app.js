e08a9d5be82657e62ff2316a52d8fb6a0d4e843e
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-10-24 16:27:22 +0700

    add shoping cart

--- src/app.js
+++ src/app.js
@@ -15,9 +15,57 @@ document.addEventListener('alpine:init', () => {
         total: 0,
         quantity: 0,
         add(newItem) {
-            this.items.push(newItem);
-            this.quantity++;
-            this.total += newItems.price;
+            // cek appakah ada barang yang sama di cart
+            const cartItem = this.items.find((item) => item.id === newItem.id);
+
+            // jika belum ada / cart masih kosong 
+            if (!cartItem) {
+                this.items.push({...newItem, quantity: 1, total: newItem.price});
+                this.quantity++;
+                this.total += newItem.price;
+            } else {
+                // jika barang sudah ada, cek apakah barang beda atau sama dengan yang ada di cart
+                this.items = this.items.map((item) => {
+                    // jika barang berbeda
+                    if (item.id != newItem.id) {
+                        return item;
+                    } else {
+                        // jika barang sudah ada, tambah quantyty dan total 
+                        item.quantity++;
+                        item.total = item.price * item.quantity; 
+                        this.quantity++;
+                        this.total += item.price;
+                        return item;
+                    }
+                })
+            }
+        },
+        remove(id) {
+            // ambil item yang mau diremove berdasarkan id 
+            const cartItem = this.items.find((item) => item.id === id);
+
+            // jika item lebih dari satu 
+            if (cartItem.quantity > 1) {
+                // telusuri 1
+                this.items = this.items.map((item) => {
+                    if (item.id != newItem.id) {
+                        return item;
+                    } else {
+                        // jika barang sudah ada, tambah quantyty dan total 
+                        item.quantity--;
+                        item.total = item.price * item.quantity; 
+                        this.quantity--;
+                        this.total -= item.price;
+                        return item;
+                    } 
+                })
+            } else if (cartItem.quantity === 1) {
+                // jika barang sisa 1
+                this.items = this.items.filter((item) => item.id != id);
+                this.quantity--;
+                this.total -= cartItem.price;
+                return item;
+            }
         }
     })
 })
