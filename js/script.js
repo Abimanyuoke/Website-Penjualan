f4685b6b98f546f946b233061808e725c88be440
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-10-24 11:48:11 +0700

    remove a script js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// toggle class active
-const navbarNav = document.querySelector(".navbar-nav");
-
-// ketika hamburger menu di click
-document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
-};
